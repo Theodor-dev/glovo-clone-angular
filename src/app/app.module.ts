4ef8278bf621139a18a9dab109b6971e49bca78a
agostino <agostino.dinuzzo@icloud.com>
2021-10-08 18:11:59 +0200

    added footer structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
